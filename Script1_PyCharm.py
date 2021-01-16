adfde9381ed19edcbe8505dba6e8dfe4b8d0f4b6
Alex Noviello <alex88ander88@yahoo.com>
2021-01-16 09:19:10 -0500

    Commit 3

---
+++ Script1_PyCharm.py
@@ -0,0 +1,20 @@
+import csv
+import json
+
+peopleCSV = open('C:/Users/alex8/Desktop/python/people.csv')
+reader3 = csv.reader(peopleCSV, delimiter=',')
+final_csv2 = list(reader3)
+region_list = []
+found = -1
+state_to_region = open('C:/Users/alex8/Desktop/python/state_to_region.json')
+state_data =  json.load(state_to_region)
+
+for x in range(len(final_csv2)):
+	for i in state_data:
+		found = final_csv2[x][3].find(i)
+		if found != -1:
+			region_list.append(state_data[i])
+			final_csv2[x][4] = state_data[i]
+			break
+
+
